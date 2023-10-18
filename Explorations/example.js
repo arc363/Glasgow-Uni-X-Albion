6d03c7f8071400ad351e25f8aba420befbd4fbd4
arc363 <148108877+arc363@users.noreply.github.com>
2023-10-18 11:33:47 +0100

    Update example.js

--- Explorations/example.js
+++ Explorations/example.js
@@ -1,6 +1,6 @@
-// process.argv.forEach((value, index) => {
-//   console.log(index, value);
-// });
+process.argv.forEach((value, index) => {
+  console.log(index, value);
+});
 
 // from digital ocean.com
 
@@ -8,24 +8,24 @@
 // argument vector is an array available from process.argv in node.js script
 // array contains everything that's passed to script, including .exe and path/filename
 
-// if (process.argv.length === 2) {
-//in the absence of an argument count
-//   console.error(`Expected at least one argument!`);
-//   process.exit(1);
-// }
+if (process.argv.length === 2) {
+in the absence of an argument count
+  console.error(`Expected at least one argument!`);
+  process.exit(1);
+}
 
 // using argument flags
 // script checks for specific item at index 2 - if present, alters the output
 
-// if (process.argv[2] && process.argv[2] === `-f`) {
-//   console.log(`Flag is present`);
-// } else {
-//   console.log(`Flag is not present`);
-// }
+if (process.argv[2] && process.argv[2] === `-f`) {
+  console.log(`Flag is present`);
+} else {
+  console.log(`Flag is not present`);
+}
 
 // can flag the actual value that's been passed to the script
-// const custom = process.argv[2] || `Default`;
-// console.log(`Custom`, custom);
+const custom = process.argv[2] || `Default`;
+console.log(`Custom`, custom);
 
 // using multiple arguments with values
 
